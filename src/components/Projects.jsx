02353830ec0c4bf8e85e8f78c2eef090a6dc7294
Borja Tapia <mrborjabtf@gmail.com>
2026-01-08 14:33:11 +0100

    changing img directory path

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -18,36 +18,36 @@ function Projects(props) {
               title="Space Alien"
               description="Juego para plataforma móvil donde eres un alíen que intenta escapar de una base subterránea. Para ello tendrás que ir esquivando los obstáculos mientras intentas ser más rápido que tus compañeros y rivales."
               downloadUrl="https://surrealboost.itch.io/secret-aliens"
-              images={["src/img/SpaceAlien.jpg", "src/img/SpaceAlien2.gif"]}
+              images={["/img/SpaceAlien.jpg", "/img/SpaceAlien2.gif"]}
             />
             <Card
               title="My Darkest Friend"
               description="Prototipo de juego de Terror donde tienes que guiarte a traves de una pistola que lanza pequeñas particulas de luz."
               downloadUrl="https://mega.nz/file/gJZDkAgL#9ZwXbnxD823beH7JADyjjJKcj6TAyzTfnZXpUocgWDE"
               images={[
-                "src/img/MyDarkestFriend.gif",
-                "src/img/MyDarkestFriend2.png",
-                "src/img/MyDarkestFriend3.png",
+                "/img/MyDarkestFriend.gif",
+                "/img/MyDarkestFriend2.png",
+                "/img/MyDarkestFriend3.png",
               ]}
             />
             <Card
               title="Cooking My Way"
               description="Prototipo de un juego de simulación de tener tu propio restaurante al puro estilo PC Building Simulator"
               downloadUrl="https://mega.nz/file/4Z5kCDYY#XZyOU0y90gQ_b8b_61xDLdTOaxvOX1YL1kxHL_41_xI"
-              images={["src/img/CookingMyWay.png"]}
+              images={["/img/CookingMyWay.png"]}
             />
             <Card
               title="Space Roar"
               description="Prototipo de juego en el espacio estilo EVERSPACE"
               downloadUrl="https://mega.nz/file/FUYB2CjZ#FAZs_byE4HKPxJMJU_HjJ2isD7rq8ttoIGkPyjzvy8k"
-              images={["src/img/SpaceRoar.png", "src/img/SpaceRoar2.png"]}
+              images={["/img/SpaceRoar.png", "/img/SpaceRoar2.png"]}
             />
             <Card
               title="One Thousand and One Fights"
               description="One Thousand and One Fights es un RTS parecido a Clash Royale donde tienes que conquistar las torres que pertenezcan al enemigo.
               Gana el primero en tener todas las torres."
               downloadUrl="https://caketowers.itch.io/one-thousand-and-one-fights"
-              images={["src/img/OTOF.jpg", "src/img/OTOFLogo.png"]}
+              images={["/img/OTOF.jpg", "/img/OTOFLogo.png"]}
             />
             <Card
               title="BlackHouse Crisis"
@@ -55,8 +55,8 @@ function Projects(props) {
               Es un shooter en vista cenital basado en la velocidad y reacción del jugador."
               downloadUrl="https://mega.nz/file/VMoUABrR#tszOWok02VBUfXrwwkH9FH4T5sUo2YgLIlQLhMP106k"
               images={[
-                "src/img/BlackHouseCrisis.png",
-                "src/img/LogoBlackHouseCrisis.png",
+                "/img/BlackHouseCrisis.png",
+                "/img/LogoBlackHouseCrisis.png",
               ]}
             />
           </div>
